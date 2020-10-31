b00e47fde93d2e28a0c0d9201e3b897f6aafe0c8
Nikita Melyanov <60012978+Nekitamr@users.noreply.github.com>
2020-10-31 08:53:20 -0400

    Skeleton commit

---
+++ NikitaTestA/src/main/java/nikita/melyanov/s301099466/MelyanovActivity.java
@@ -0,0 +1,7 @@
+package nikita.melyanov.s301099466;
+
+// Nikita Melyanov s301099466
+
+public class MelyanovActivity {
+
+}
---
+++ NikitaTestA/src/main/java/nikita/melyanov/s301099466/NikitaActivity.java
@@ -0,0 +1,16 @@
+package nikita.melyanov.s301099466;
+
+// Nikita Melyanov s301099466
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NikitaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
